514cd2c977cdbc745bc336199dd4d789976963ab
Sylvan Brocard <sylvan.brocard@gmail.com>
2021-03-31 14:10:57 +0200

    added Engine class skeleton

---
+++ engine.py
@@ -0,0 +1,18 @@
+class Engine:
+    def __init__():
+        pass
+    def start_game(self):
+        pass
+    def print_board(self):
+        pass
+    def get_input(self):
+        pass
+    def resolve_move(self,input):
+        pass
+    def is_finished(self):
+        pass
+    def end_game(self):
+        pass
+
+if __name__='__main__':
+    pass
