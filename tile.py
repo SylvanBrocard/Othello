67b8e58a56de72cf3547893fbc4fe8ea31f4717f
Laurent Lebreton <laurent.lebreton@le-campus-numerique.fr>
2021-03-31 16:58:42 +0200

    livraison

---
+++ tile.py
@@ -0,0 +1,14 @@
+from pawn import *
+
+class tile():
+    
+    def __init__(self,x,y):
+        self.__x = x
+        self.__y = y
+        
+
+    def get_coordonates():
+        return self.__x, self.__y
+
+    
+        
