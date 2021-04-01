8701c4f2f9c39e69014aaa87764eb83eebc5b956
skpai <suresh.sasidharan-pillai@le-campus-numerique.fr>
2021-04-01 17:00:23 +0200

    changes to Board

---
+++ common/constants.py
@@ -0,0 +1,4 @@
+symbol_black = 'o'
+symbol_white = '*'
+number_of_ranks = 8
+number_of_files = 8
---
+++ pawn.py
@@ -0,0 +1,9 @@
+class Pawn():
+    def __init__(self, color):
+        self.color = color
+
+    def reverse_pawn(self):
+        if self.color == 'o':
+            self.color = '*'
+        elif self.color == '*':
+            self.color = 'o'
